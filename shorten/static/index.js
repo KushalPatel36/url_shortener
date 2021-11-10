30fc57baf00599c65489ae44383a8fb3a46ac506
KushalPatel36 <kushal.patel@hotmail.co.uk>
2021-11-10 15:50:32 +0000

    functionality working

--- shorten/static/index.js
+++ shorten/static/index.js
@@ -16,11 +16,48 @@ function getShort(e) {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-           
+
         },
 
         body: JSON.stringify(data),
     })
-        .then(response => response.json())
-        .then(data => console.log(data))
+        .then(r => r.json())
+        .then(r => r.path)
+        .then(r => addNew(r))
+
+
+
+    function addNew(u) {
+        console.log(u)
+        let newURL = document.createElement("a")
+        console.log(u)
+        newURL.textContent = `http://localhost:8000/url/${u}`
+        console.log(newURL.textContent)
+        document.getElementById('mainBody').appendChild(newURL)
+
+
+        fetch(newURL.textContent)
+            .then(r => r.json())
+            .then(r => r.url)
+            .then(r => linkButton(r))
+
+
+
+        function linkButton(m) {
+            let newButton = document.createElement("a")
+            console.log(m)
+            newButton.textContent = 'Visit your link'
+            newButton.href = m
+            newButton.setAttribute('class', 'myNewButton')
+            console.log(newButton.textContent)
+            document.getElementById('mainBody').appendChild(document.createElement("br"))
+            document.getElementById('mainBody').appendChild(newButton)
+        }
+
+
+
+    }
+
+
 }
+
