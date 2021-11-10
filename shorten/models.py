67a362d1691d6cc4745e1e68c3c07af235b2a7c1
Arafath Miah <miah.arafath@gmail.com>
2021-11-10 11:26:46 +0000

    API takes url in body and generates a path and saves the url in the database

--- shorten/models.py
+++ shorten/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Url(models.Model):
+
+    shortened = models.CharField(max_length=6)
+    redirect = models.CharField(max_length=1023)
+
+    class Meta:
+        verbose_name = "Url"
+        verbose_name_plural = "Urls"
+
+    def __str__(self):
+        return self.redirect
--- shorten/views.py
+++ shorten/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.utils.crypto import get_random_string
 
+from .models import Url
 # Create your views here.
+
+
+def generate_random_id():
+    return get_random_string(length=6)
+
+
+@csrf_exempt
+def short(request):
+    if request.method == 'POST':
+
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        url = body['url']
+
+        random_id = generate_random_id()
+
+        while True:
+            try:
+                Url.objects.get(shortened=random_id)
+                random_id = generate_random_id()
+            except Url.DoesNotExist:
+                break
+
+        print(random_id)
+
+        url_to_save = Url(redirect=url, shortened=random_id)
+        url_to_save.save()
+
+        response = {
+            "status": "success",
+            "path": random_id
+        }
+
+        return JsonResponse(response)
+
+    return JsonResponse({"satuts": "FAILED"})
