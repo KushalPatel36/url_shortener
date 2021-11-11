95dc66f040bd7cd16c225de0681f22d87fb2c22f
KushalPatel36 <87951070+KushalPatel36@users.noreply.github.com>
2021-11-11 09:36:29 +0000

    Delete shorten directory

--- shorten/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class SubmitUrl(forms.Form):
-    url = forms.CharField(label="Submit URL",
-                          widget= forms.TextInput(attrs={"placeholder":"Long URL",
-                                                         "class":"form-control"}))
--- shorten/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.short),
-    path('home', views.home),
-    path('<str:path>', views.get_full_url),
-]
